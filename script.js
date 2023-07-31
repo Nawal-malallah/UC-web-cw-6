2f0ac2408934c596e5b4c82b1f7b477b98214c8a
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2023-07-31 17:34:27 +0300

    i had trouble with this

---
+++ script.js
@@ -0,0 +1,32 @@
+function BMI (weight, height) {
+    let result = weight / (height * height);
+    return result;
+}
+
+console.log(BMI (70, 1.7));
+
+function Status(BMI) {
+    if (BMI < 18.5){
+    return ("you are under weight")
+}
+
+    else if (BMI >= 18.5 && BMI < 25)
+        return "you are healthy"
+
+    else {
+        return "you are overweight"
+    }
+
+}
+
+function calculate() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+    let bmi = BMI(weight , height);
+    
+    let desc = Status(bmi);
+    let div = document.getElementById("result");
+
+    div.innerText = bmi + " == " + desc;
+}
