1e6f73c20644886a9ca132ac2909e51da0852c20
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2023-08-03 13:01:22 +0300

    checkpoint

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function BMI (weight, height) {
+function BMI(weight, height) {
     let result = weight / (height * height);
     return result;
 }
 
-console.log(BMI (70, 1.7));
+console.log(BMI(70, 1.7));
 
 function Status(BMI) {
-    if (BMI < 18.5){
-    return ("you are under weight")
-}
+    if (BMI < 18.5) {
+        return ("you are under weight")
+    }
 
     else if (BMI >= 18.5 && BMI < 25)
         return "you are healthy"
@@ -23,8 +23,8 @@ function calculate() {
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
 
-    let bmi = BMI(weight , height);
-    
+    let bmi = BMI(weight, height);
+
     let desc = Status(bmi);
     let div = document.getElementById("result");
 
